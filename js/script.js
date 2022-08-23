2427ab3a897d0367aa2c9a4bf3981db14ccf4e4a
zahid4523114 <dewanmohammodzahidhasan@gmail.com>
2022-08-22 19:03:23 -0600

    event listener added to player btn

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
-//player-1
+//player-1-btn
 document.getElementById("player-btn-1").addEventListener("click", function () {
   setListElementById("all-list-element", "Neymar Jr");
   buttonDisabled("player-btn-1");
 });
-//player-2
+//player-2-btn
 document.getElementById("player-btn-2").addEventListener("click", function () {
   setListElementById("all-list-element", "Kylian Mbappé");
   buttonDisabled("player-btn-2");
 });
-//player-3
+//player-3-btn
 document.getElementById("player-btn-3").addEventListener("click", function () {
   setListElementById("all-list-element", "Lionel Messi");
   buttonDisabled("player-btn-3");
 });
-//player-4
+//player-4-btn
 document.getElementById("player-btn-4").addEventListener("click", function () {
   setListElementById("all-list-element", "Sergio Ramos");
   buttonDisabled("player-btn-4");
 });
-//player-5
+//player-5-btn
 document.getElementById("player-btn-5").addEventListener("click", function () {
   setListElementById("all-list-element", "Renato Sanches");
   buttonDisabled("player-btn-5");
 });
-//player-6
+//player-6-btn
 document.getElementById("player-btn-6").addEventListener("click", function () {
   setListElementById("all-list-element", "Vítor Machado");
   buttonDisabled("player-btn-6");
